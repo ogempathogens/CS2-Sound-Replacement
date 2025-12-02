e3266a85157102cfd80493c02a56a5893a150ad1
Empathogens <chiefkeef.pl@gmail.com>
2025-12-02 23:23:55 +0100

    remove useless hardcoded prints

--- FNSearch.py
+++ FNSearch.py
@@ -7,13 +7,3 @@ slot_22_addr = vtable_start + 0xB0
 
 func_addr = ida_bytes.get_qword(slot_22_addr)
 ida_kernwin.jumpto(func_addr)
-
-signature = "__int64 __fastcall (__int64 this, int index)"
-idc.SetType(func_addr, signature)
-
-idc.set_name(func_addr, "CVSoundTypeManager_GetResource", idc.SN_NOWARN)
-
-print("\n")
-print(f"Addr: 0x{func_addr:X}")
-print(f"Function: {signature}")
-print(f"Matches desired function: __int64 func(__int64 this, int index)")
