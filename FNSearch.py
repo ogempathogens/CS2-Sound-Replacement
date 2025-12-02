c6a02d0159ec08bf4c2cd4402893974a8e45206a
Empathogens <chiefkeef.pl@gmail.com>
2025-12-02 23:25:20 +0100

    forgot to remove useless imports

--- FNSearch.py
+++ FNSearch.py
@@ -1,9 +1,5 @@
-import ida_bytes
-import ida_kernwin
-import idc
-
 vtable_start = 0x180380B60
 slot_22_addr = vtable_start + 0xB0
-
 func_addr = ida_bytes.get_qword(slot_22_addr)
+
 ida_kernwin.jumpto(func_addr)
